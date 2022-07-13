5fdf04cdb1d1daf2338f7a6ef8a8285c295e34cf
Kai De Windt <93126275+dewindtk@users.noreply.github.com>
2022-07-14 00:52:41 +0200

    Update bot.js
    Cleaned up a bit removed the gas estimations lmk lmk :))

--- bot.js
+++ bot.js
@@ -21,80 +21,58 @@ function sleep(ms) {
   });
 }
 
-let i = 0;
 
-async function buy() {
-  try {
-    console.log("starting up ...")
-    
-    while (i == 0) {
-      let response = await axios.get("https://wzrds.xyz/peddler/targets");
-      if (response.data.targets.length != 0) {
-        console.log("new listing to burn found");
-        console.log(response.data.targets[0]);
 
-        let firstPart = web3.eth.abi.encodeFunctionSignature(
-          `assassinate(bytes,uint256,uint256,address,uint256,uint256)`
-        );
-        let secondPart = web3.eth.abi.encodeParameters(
-          ["bytes", "uint256", "uint256", "address", "uint256", "uint256"],
-          [
-            response.data.targets[0].proof,
-            response.data.targets[0].token_id.toString(),
-            "20000000000000000000000",
-            response.data.targets[0].owner_address,
-            response.data.targets[0].expires_at.toString(),
-            response.data.targets[0].nonce.toString(),
-          ]
-        );
-        const hexData = firstPart + secondPart.slice(2);
-        console.log(hexData);
-        const estGas = await provider.estimateGas({
-          from: publicAddy,
-          to: "0x6A27Ed5248D0557D016C0aA2D5FFa59fe273E666",
-          data: hexData,
-          maxFeePerGas: ethers.utils.parseUnits("100", "gwei"),
-          maxPriorityFeePerGas: ethers.utils.parseUnits("100", "gwei"),
-        });
-        console.log(estGas.toNumber());
-        if (estGas.toNumber() <= 200000) {
-          const transaction = await web3.eth.accounts.signTransaction(
-            {
-              to: "0x6A27Ed5248D0557D016C0aA2D5FFa59fe273E666",
-              value: ethers.utils.parseEther("0"),
-              data: hexData,
-              maxFeePerGas: ethers.utils.parseUnits(maxFee, "gwei"),
-              maxPriorityFeePerGas: ethers.utils.parseUnits(maxPrioFee, "gwei"),
-              gas: 180000,
-            },
-            privKey
-          );
-          web3.eth.sendSignedTransaction(
-            transaction.rawTransaction,
-            function (error, hash) {
-              if (!error) {
-                console.log("🎉 The hash of your transaction is: ", hash);
-                i = 1;
-              } else {
-                console.log(
-                  "❗Something went wrong while submitting your transaction:",
-                  error
-                );
-                i = 1;
-              }
-            }
-          );
-        } else {
-          console.log("prob oos");
-        }
-      } else {
-        console.log("no new listings to burn");
-      }
+async function buy() 
+{
+  console.log("starting up ...")
+  let firstPart = web3.eth.abi.encodeFunctionSignature(
+    `assassinate(bytes,uint256,uint256,address,uint256,uint256)`
+  );
 
-      sleep(100);
+  while (1) 
+  {
+    let response = await axios.get("https://wzrds.xyz/peddler/targets");
+    toSnipe = response.data.targets[0]
+    if (response.data.targets.length == 0) 
+    {
+      console.log("no new listings to burn");
+      await sleep(100);
+      continue;
     }
-  } catch (error) {
-    console.log(error)
+
+    console.log("new listing to burn found");
+    console.log(toSnipe);
+
+    let secondPart = web3.eth.abi.encodeParameters(
+      ["bytes", "uint256", "uint256", "address", "uint256", "uint256"],
+      [
+        toSnipe.proof,
+        toSnipe.token_id.toString(),
+        "20000000000000000000000",
+        toSnipe.owner_address,
+        toSnipe.expires_at.toString(),
+        toSnipe.nonce.toString(),
+      ]
+    );
+    const hexData = firstPart + secondPart.slice(2);
+    console.log("Your hex data", hexData);
+    const transaction = await web3.eth.accounts.signTransaction(
+      {
+        to: "0x6A27Ed5248D0557D016C0aA2D5FFa59fe273E666",
+        value: ethers.utils.parseEther("0"),
+        data: hexData,
+        maxFeePerGas: ethers.utils.parseUnits(maxFee, "gwei"),
+        maxPriorityFeePerGas: ethers.utils.parseUnits(maxPrioFee, "gwei"),
+        gas: 180000,
+      },
+      privKey
+    );
+    console.log("Sending Tx now")
+    web3.eth.sendSignedTransaction(transaction.rawTransaction).on('receipt', console.log);
+    return 0;
   }
 }
+
+
 buy();
